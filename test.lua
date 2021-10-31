69dd0b13b255ba31d3b1d0daa825c08fd06e2588
Christopher Moore <christopher.e.moore@gmail.com>
2021-10-31 13:04:34 +0600

    test program now formats output

--- test.lua
+++ test.lua
@@ -1,5 +1,7 @@
 #!/usr/bin/env luajit
 local NetCDF = require 'netcdf'
+local range = require 'ext.range'
+local table = require 'ext.table'
 
 local fn = assert(..., "expected filename")
 local netcdf = NetCDF{filename=fn}
@@ -17,11 +19,25 @@ for _,var in ipairs(netcdf.vars) do
 	end
 end
 
-print'values:'
+-- gather cols beforehand, find the max, format accordingly.  extra +1 for indexes
+local cols = range(#netcdf.vars+1):mapi(function() return table() end)
 for i in netcdf:indexes() do
-	io.write(table.concat(i, ','))
-	for _,var in ipairs(netcdf.vars) do
-		io.write('\t', tostring(var:get(table.unpack(i))))
+	cols[1]:insert(table.concat(i, ','))
+	for j,var in ipairs(netcdf.vars) do
+		cols[j+1]:insert(tostring(var:get(table.unpack(i))))
+	end
+end
+
+
+print'values:'
+local maxs = cols:mapi(function(col) return (col:mapi(function(s) return #s end):sup()) end)
+for i=1,#cols[1] do
+	for j,col in ipairs(cols) do
+		if j > 1 then
+			io.write'   '
+		end
+		local s = col[i]
+		io.write(s..(' '):rep(maxs[j]-#s))
 	end
 	print()
 end
